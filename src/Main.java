6ec10703b38c5f9b9a3a9dee60155e1692185ce6
Lukin <Luk.bc@yandex.ru>
2023-01-20 17:03:39 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 1000;
+        int rublePerMile = 20;
+
+        int bonusMiles = ticketPrice / rublePerMile;
+        System.out.println((bonusMiles) + " бонусных миль");
+
+
+
+
+
+
+
+    }
+}
